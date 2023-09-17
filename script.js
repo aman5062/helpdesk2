b781cbe83abe5d29dddf0aaf6e0bbc875462fed4
aman5062 <aman9122701848@gmail.com>
2023-09-17 11:10:34 +0530

    all

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 function myFunction(){var a=document.getElementById("myTopnav");"topnav"===a.className?a.className+=" responsive":a.className="topnav";var s=document.getElementById("bar");"fa fa-bars"===s.className?s.className="fa fa-times":s.className="fa fa-bars"}
+// JavaScript in script.js or within <script> tags in HTML
+
+window.addEventListener('scroll', function () {
+    const goToTopButton = document.getElementById('goToTopButton');
+    if (window.scrollY > 300) { // Adjust this value based on your needs
+        goToTopButton.style.display = 'block';
+    } else {
+        goToTopButton.style.display = 'none';
+    }
+});
+
+document.getElementById('goToTopButton').addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
