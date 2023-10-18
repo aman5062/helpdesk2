74b9cd5faf8b7f5c9e4dd4b17a86c94757a7b9bc
mmakiesse <mmakiesse82@gmail.com>
2023-10-18 10:00:14 +0100

    header UI change

--- script.js
+++ script.js
@@ -10,9 +10,48 @@ window.addEventListener('scroll', function () {
     }
 });
 
-
-
- window.addEventListener("load",function(){
+window.addEventListener("load",function(){
     var loader = document.getElementById("preprocessor");
     loader.style.display = "none";
- })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('#myNav .nav-link');
+    // Get the current URL path without the domain
+    const currentPath = window.location.pathname;
+
+    // Loop through the navigation links
+    navLinks.forEach(link => {
+        const linkHref = link.getAttribute('href');
+        const isCurrentPage = currentPath.endsWith(linkHref);
+
+        // Check if the current page matches the link's href attribute
+        if (isCurrentPage) {
+            link.classList.add('active');
+            link.classList.remove('text-black');
+        } else {
+            link.classList.remove('active');
+            link.classList.add('text-black');
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdown = document.querySelector("#myDropdown");
+    const dropdownToggle = dropdown.querySelector(".dropdown-toggle");
+    const dropdownMenu = dropdown.querySelector(".dropdown-menu");
+    const dropdownItems = dropdownMenu.querySelectorAll(".dropdown-item");
+
+    dropdownToggle.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        // Toggle the 'show' class to expand/collapse the dropdown menu
+        if (!dropdownMenu.classList.contains("show")) {
+            dropdownMenu.classList.add("show");
+        } else {
+            dropdownMenu.classList.remove("show");
+        }
+    });
+
+});
