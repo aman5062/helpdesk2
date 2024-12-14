ec5a08bae384831ac492e3e835ed11a831d5854a
Aman kumar keshri <72082692+aman5062@users.noreply.github.com>
2024-12-14 11:14:09 +0530

    dd

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,3 +121,28 @@ function deleteAllCookies() {
 
 // Call the function to delete all cookies
 // deleteAllCookies();
+function waterm(){
+    let n = getCookie("name");
+    if(n!=null){
+      document.getElementById("watermark").innerHTML = n;  
+    }
+    else{
+        document.getElementById("watermark").innerHTML = "@Debuggers";  
+    }
+    
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
